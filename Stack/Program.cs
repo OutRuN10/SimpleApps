7233eb8938746dfb8b437dda5939de9f7105794c
OutRuN10 <37771743+OutRuN10@users.noreply.github.com>
2018-04-01 21:40:14 +0300

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackWithFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            var max = 5;
+            var start = 0;
+
+            for (var i =start; i<=max; i++)
+                stack.Push(i);
+
+            
+
+            for (var i = start; i <= max; i++)
+                Console.WriteLine(stack.Pop());
+
+            
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Schema;
+
+namespace StackWithFor
+{
+    public class Stack
+    {
+        private readonly List<object> _mylist = new List<object>();
+
+        // Add an item on the list
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Invalid Object");
+            
+             _mylist.Add(obj);
+        }
+
+        // Remove an item from the list
+
+        public object Pop()
+        {
+            if (_mylist.Count == 0)
+                throw new InvalidOperationException("Empty List");
+
+            var returnedValue = _mylist.Count - 1;
+            _mylist.RemoveAt(_mylist.Count -1);
+
+            return returnedValue;
+        }
+
+        //To clear the list //
+
+        public void Clear()
+        {
+            _mylist.Clear();
+            
+        }
+
+    }
+}
