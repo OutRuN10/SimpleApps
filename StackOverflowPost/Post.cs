3989be0868f0585fff0a34a2c5a1e30e268f66ef
OutRuN10 <37771743+OutRuN10@users.noreply.github.com>
2018-04-01 21:08:51 +0300

    Add files via upload

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace StackOverFlowPost
+{
+    class Post
+    {
+        private DateTime _date;
+        private int _count = 0;
+       
+
+        public string Title { get; set; }
+
+
+        public void datetime()
+        {
+            _date = DateTime.Now;
+            
+        }
+
+        public void voting()
+        {
+            while (true)
+            {
+                Console.Write("For Upvote press 1 and for downvote press 2(or press Enter to exit): ");
+                var input = Console.ReadLine();
+                if (input == "1")
+                    _count++;
+                if (input == "2")
+                    _count--;
+                if (input !="1" || input != "2" || !String.IsNullOrWhiteSpace(input))
+                    Console.WriteLine("Invalid entry,retry");
+                
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+            }
+            Console.WriteLine("The post with the name {2} was created on {0} and got a total of {1} vote value", _date, _count, Title);
+            
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverFlowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Post title: ");
+            var newpost = new Post();
+            newpost.Title = Console.ReadLine();
+            newpost.datetime();
+            newpost.voting();
+            
+
+
+        }
+    }
+}
