7bbfef6c24f3c8399db9a75a4cbd63a4c157a983
OutRuN10 <37771743+OutRuN10@users.noreply.github.com>
2018-04-01 21:39:07 +0300

    Add files via upload

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+            var duration = new mystopwatch();
+
+
+
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Press start to start the stopwatch or enter to exit: ");
+                    var input1 = Console.ReadLine();
+                    if (input1.ToLower() == "start")
+                    {
+                        count++;
+                        duration.Start();
+
+                        if (count > 2)
+                            duration.Continue();
+                    }
+                    if (String.IsNullOrWhiteSpace(input1) || input1.ToLower() != "start")
+                        break;
+                    Console.Write("Press stop to stop the stopwatch: ");
+                    var input2 = Console.ReadLine();
+                    if (input2.ToLower() == "start")
+                        throw new InvalidOperationException();
+
+                    if (input2.ToLower() == "stop")
+                        duration.Stop();
+                    else break;
+
+                }
+                Console.WriteLine("The duration is {0}", duration.Calculate());
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You cannot restart the stopwatch before stopping it");
+            }
+            
+
+
+            
+            
+
+
+        }
+    }
+}
---
+++ Stopwatch/mystopwatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace StopWatch
+{
+    public class mystopwatch
+    {
+        private DateTime _now1;
+        private DateTime _now2;
+        private TimeSpan Calc;
+
+        public void Start()
+        {
+            _now1 = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            _now2 = DateTime.Now;
+
+        }
+
+        public void Continue()
+        {
+
+        }
+
+        public TimeSpan Calculate()
+        {
+            return _now2 - _now1;
+        }
+    }
+}
